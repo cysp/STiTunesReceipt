49096eb20fc56db9240308693077c408a10c8b05
Scott Talbot <s@chikachow.org>
2014-02-24 21:37:02 +1100

    Import skeleton

---
+++ STiTunesReceiptValidator/main.m
@@ -0,0 +1,16 @@
+//  Copyright (c) 2014 Scott Talbot. All rights reserved.
+
+@import Foundation;
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
